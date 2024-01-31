c0734abc4dd25c31555461e06fe7d8416988d8aa
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-01-31 17:12:23 +0000

    first commit

---
+++ kickoff warmup.py
@@ -0,0 +1,6 @@
+"written as a string data type = this is my answer/response...
+"strings use quotation marks"
+#written as a comment = This is my response.answer..."
+# comments use the hashtag
+# I think a computer program is a way that a computer operates, for example I think what we are doing right now is a program to write in python like how Ian was explaining.
+"My favorite food is Cheesecake, to make cheesecake theres simple instructions. Make a filling from cream cheese, eggs, sugar and vanilla. Then I like to make my crust from biscoff cookies and then bake it 55-70 minutes on 250 F* (121*C).For the lighter, darker-colored cheesecake, cook the cheesecake for 20 minutes at high heat, then turn off the oven and prop the door open with a wooden spoon for 10 minutes.A bain-marie, or water bath, is the preferred method for baking cheesecakes because it gently cooks the eggs in the filling."
