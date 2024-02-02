8b5c4a9d4a7329b1d78c2da8dbe503cbb9bfb38a
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-02-02 17:49:01 +0000

    Feb, 2nd Daily Activity Completed

---
+++ dataTypeLesson.py
@@ -0,0 +1,11 @@
+# Functions are just instructions that do a job.
+
+# Take in user data
+name=input('What is your name: what is your age:')
+age=input('what is your age: "')
+location=input("Where do you live: ")
+
+# Puts out data
+Print(name)
+print(age)
+print(location)
---
+++ feb1Activity.py
@@ -0,0 +1,20 @@
+"a variable is something that stores the data for you, they're used as labels so that you know what something is when you're searching for it."
+"An interger or an int is a number without the decimal and float is a number with a decimal."
+"Bolean data is data that can either be true or false."
+
+# - 1 Variable for your full name
+name = "Brandon Jones"
+
+# - 1 variable for your age
+age = 17
+
+# - 1 Variable for your GPA
+GPA = 3.08
+
+# - 1 variable for if you are a student of Boys Latin
+#boolean
+
+amIaStudent = True
+
+"Once I was stranded in Northeast Philadelphia and I didn't know where I was but when I finally realized that I could just look at the maps and use the maps to tell me what bus I should be taking I then knew how to get home. It ended up taking me 2 hours to get home from where I was but I know if I didn't have that technology it would've taken me over that to get home."
+"We all use technology on a daily basis and when I lose my phone for 10 minutes I'm already in distraught. I know if I lost my phone forever and ever and technology in general it was feel impossible to adapt but as humans we will figure out how to live without the technology and it could even make us better. I think I would have to start working out and finding ways of entertainment in otherways that aren't in technology. The entertainment part wouldn't be hard for me since I'm big on sports, I love to play basketball and even though I couldn't watch basketball on my TV I could go watch games at a local park and even join in to play with my peers so that we are getting physical actvity in and also having fun with each other."
