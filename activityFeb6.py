26144e400a672f49b7c62dd53544d02c99ce5551
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-02-07 16:58:50 +0000

    Feb 7th, 2024 Assignment Completed

---
+++ activityFeb6.py
@@ -0,0 +1,57 @@
+# Q : What is complexity?
+# A : What happens behinds the scenes. Is the measure of resources a program uses.
+# Resource = Time, Data, and Energy.
+
+# Q : What is abstraction?
+# A : The process of simplifying or hiding complexity.
+
+# ! : What is a Variable?
+# A : A container for data.
+
+passcode = imsohungryrn02062024
+visa = 000000 0000 000000 0000
+
+# Data is stored in variables. This is an example of abstraction
+Customer_ID_1 = 'Brandon Jones'
+Customer_ID_2 = 
+Customer_ID_3 = 
+
+VisaCard_ID_1 = '02032006'
+VisaCard_ID_2 = 
+VisaCard_ID_3 = 
+
+# Rules of naming variables
+# 1. Variables name are short and descriptive.
+# 2. Variablenames can start with a letter 
+# or an unerscore (_).
+
+# 3. Variables CANNOT start with numbers
+# or non-alphabetical characters
+
+# 8Student_ID = ''
+# *Student_ID = ''
+
+#. Vairiables cannot be Python Keywords.
+
+
+#camelcase = 
+educationSectionPhysics = ''
+educationSectionMath = ''
+EducationSectionLiterature = ''
+
+# Pascal Case = Every word
+# Including the first word 
+
+# Arithmatic Operators = Arithmatic Actions
+print(2+2)
+print(100/2)
+print(400*20)
+
+# Assignment operators = assignment actions
+
+name = 'IAN'
+payment = 100
+
+x = 100
+x + = 3
+print(x)
---
+++ activityFeb7.py
@@ -0,0 +1,27 @@
+1.
+# Q : In your words, what is a computer program?
+# A : A set of orders on a computer for the computer to do.
+
+2.
+# Q : Name three data types
+# A : 1. Boolean, Integer, and String
+
+3.
+# Q : Name three Python Operators
+# A : Arithmetic, Assignment, and Relational
+
+4.
+# Q : In your own words what is a variable?
+# A : A container
+
+5.
+# Q : You have been hired as a software engineer to develop a gym membership system. Your program needs to verify two (2) things; if the user is a member of the gym, AND  if the user has the gold membership tier, which allows the user to have access to the swimming pool.  What your client would like to happen is that when a user is a member and also has the gold membership they should be able to use the pool. Which python operator would you use in this situation? Explain why
+# A : I'd use Relational Python Operator for a gym membership. Relational shows a connection between one thing and another thing, this would show a connection to your card being a member and having the gold membership tier on your car allowing you to have access to all the things that come with it.
+
+6.
+# Q : # 6. You have been hired as a software engineer to develop a simple payment system for a companys webstore. Your assignment is to create a cart system where the prices of all the items are added together and then deducts a 20% discount at checkout. Which python operator would you use in this situation? Explain why. 
+# A : I'd use the Artihmatic Python Operator for a discount software for a store. I'd use Arithmatic due to its math like structure, using multiple variables like addition, subtraction, multiplication, and division. This would give it the 20% discount a breeze by automatically giving what you buy the discount immedietely and helping with the hassle.
+
+7.
+# Q : # 7. You have been hired as an engineer to develop a student badge system. Your client would like to have a system where when a student swipes their badge, the ID number on the badge is compared to the ID number in the database. If the ID numbers match and are the same the student can come in, if it does not match, they cannot enter. Which python operator would you use in this situation? Explain why
+# A : Last but not least I'd use the Assignment Python Operator for this task. This would use its equal sign feature to use. Everytime your student badge equaled to the system you would be allowed to go into the building but if it didn't equal what was in the system you would be denied.
