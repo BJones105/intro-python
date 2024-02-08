df6c12ca7e35b15a3cc4dae648a53f9a8ef08269
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-02-08 17:14:47 +0000

    Feb 8th, 2024 Completed

---
+++ Notes/ActivityFeb8.py
@@ -0,0 +1,22 @@
+# Using the tool's we've learned in class up to this point, create a block of code that will return a value.
+# (You can use variables, data types, operators, etc)
+
+# 1. Create a block of code that uses the any of the arithmetic operators then print out the result.
+# A : 
+
+# input always transforms the data into strings
+input('give me data')
+print('printing out data')
+print(54 + 68)
+number_A = 54
+number_B = 68
+print(number_A + number_B)
+
+# 2. Create a block of code that uses the and operator and then print out the result.
+# A :
+bankAccount = 100.00
+
+deposit = float(input('how much would you likee to deposit?'))
+                
+print('your bank total')
+print(bankAccount + deposit)
---
+++ Notes/NotesFeb8.py
@@ -0,0 +1,10 @@
+# Data 
+bankAccount = 100.00
+# the input function
+# automatically transform data into strings.
+
+# with data casting 
+deposit = float(input('how much would you likee to deposit?'))
+                
+print('your bank total')
+print(bankAccount + deposit)
