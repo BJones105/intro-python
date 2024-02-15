3e988fe616b77029a3d2f804e603e315ea6b4337
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-02-15 16:31:55 +0000

    Feb. 14th Assignment Completed

---
+++ Notes/AcitvityFeb14.py
@@ -0,0 +1,22 @@
+# 1. In your own words, what is a built-in function?
+# A : A built-in function are functions to perform diff. tasks.
+# a : Examples -
+input()
+str()
+int()
+# 2. Name and describe one built in function we've used in class?
+input() # a : input is a built-in function, it stores the data in a variable.
+
+# 3. What type of data can be assigned inside of a python list.
+groceryList =[amazonList, 'cereal', 10.233, 29, '19390oid.390r.3298ru']
+
+def peanutButterAndJelly():
+    print('Ingridents: peanut butter, jelly, 2 slices of bread')
+    print('step 1. lay he two pieces of read flat next to one anoter')
+    print('step 2. spread peanut butter on one piece of bread')
+    print('step 3. spread jelly on the other piece of bread')
+    print('step 4. put both sides together with the peanut butter and jelly together')
+
+peanutButterAndJelly()
+
+def CheeseSteak
