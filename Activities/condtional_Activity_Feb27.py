65f6b3b8e501983559292a48f8f9709c5c2d2b77
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-02-27 17:56:52 +0000

    Feb27th Completed Homework

---
+++ Activities/condtional_Activity_Feb27.py
@@ -0,0 +1,9 @@
+def main():
+    while True:
+        PassWord = input ("Enter Password: ")
+
+        if PassWord == "get_this_money":
+            print ("Login successful!")
+
+        else:
+        print ("Password did not match!")
