786cfd5c63d1c1e9d1bff2533064095a3530c829
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-02-27 16:33:15 +0000

    All Work Submitted

---
+++ Notes/Feb22Notes.py
@@ -0,0 +1,33 @@
+# function - A set of instructions that exeute one task in a program.
+
+
+# Functions have 2 parts :
+# 1. Function Definiton - The instruction on how the function works for the program.
+
+def goodAfternoon():
+    print('Good afternoon team!')
+
+# 2. function call : tells the program
+# to execute/run our function instructions.
+    
+goodAfternoon()
+
+# Function Parameters and Arguments - external data passed into a function.
+# data type = strings, floats, boolean, etc. data base
+
+def giftCard_add5(user_Giftcard):
+    newbalance= user_Giftcard +5
+    print(f'your new gift card balance is {newbalance} dollars.')
+
+giftCard_add5(12)
+
+def giftCard_add10(user_Giftcard):
+    newbalance= user_Giftcard + 10
+    print(f'your new gift card balance is {newbalance} dollars.')
+
+    giftCard_add10(55
+                   
+def salary_120k():
+    # write some instructions
+    print('here are 120k jobs...')
+
---
+++ Quiz/Feb26Quiz.py
@@ -0,0 +1,82 @@
+# 1. What built-in function would you use if you wanted to print out data to a user?
+#  print()
+
+# 2. What built-in function would you use if you wanted a user to pass in daya to a program?
+# input()
+
+# 3. Explain what complexity and abstraction are
+# Provide an example descrbing their relationship to one another
+# You cannot use the following examples because they were shared in class; grocery/electronics store, car, phone.
+
+'Complexity is the measure of resource intensive a program can be.'
+'Abstraction is the process of hiding/masking complexity'
+
+# 4. What does it mean when you encounter a syntax error?
+
+'This error occurs when you are not using the proper grammar/ and or language rules of python'
+
+# 5. What is a variable? Provide an example of  variable in code.
+# A variable is a container for data.
+'Container for data.'
+name = 'Brandon'
+password=
+
+'Brandon'
+30
+30.00
+True
+()
+{}
+[]
+
+# 6. Write a function in Python that when called will print out your name.
+
+def whatsyourname():
+    name =input('Brandon: ')
+    print(name)
+    age = input('18')
+    print(age)
+    print('hi my name is 'Brandon', and my age is '18'')
+
+# 7. What is a function definition and a function call?
+'Function Definition -> Instructions that will only run when called.'
+
+# 8. Describe what each symbol means?
+ # = // signal equal
+'Single equal sign = assignment'
+career = 'engineer'
+score = 60
+
+# = // singal equal
+'Single equal sign = assignemnts'
+career = 'engineer'
+score = 60
+
+# == //
+
+# ! // exclamation equal
+'Comparison operator, we use it to compare if values are the same'
+
+#9 What will the output for the following code block be?
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+print (listOne == listTwo)
+
+'True = both lists are the same'
+
+# 11. Create the following python lists and incude the following data types for each list:
+bookBagItemPrice = [1.00, 2.00, 3.00, .50]
+
+# 12. What are the Three (3) naming formats we use for naming variables
+'camelCase - The first letter in the first word is lowercase, but every new word after is uppercased.'
+'PascalCase - every word is uppercased.'
+'snake_case - every word is seperated with an underscore'
+
+# 13. Provide a coding scenario where you would use a comparison
+
+
+var1 = 1
+var2 = 2
+var3 = "3"
+
