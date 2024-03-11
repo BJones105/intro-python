72f083840beba8e201de78751696fd314850a338
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-03-11 17:14:00 +0000

    ASync March, 11th Completed

---
+++ Activities/async_3_11.py
@@ -0,0 +1,44 @@
+# INSTRUCTIONS
+# Create a new python file called async_3_11.py
+# Answer the following questions. Once you've completed the questions, commit
+# and sync your work.
+# This activity must be completed by the end of class in order to recieve
+# a grade.
+
+# REMEMBER - TAKE YOUR TIME AND DO YOUR BEST! DO NOT GIVE UP! 
+
+# 1. Which Python datacasting function would
+# you use if you wanted to convert a string data type
+# into an integer data type? Please write your response
+# in a complete sentence. 
+
+# a : An atoi() function is used to convert the character string value into an integer value.
+
+# 2. Create a list called numbCol that contains three (3 ) colors and three (3) numbers.
+
+numbcol = color, number
+
+number = [1,2,3]
+color = [red, yellow, green]
+
+# 3. You have been hired by a University to create
+# a scholarship function. The client would like to provide 
+# students a scholarship to their school based on the following
+# conditions; 
+# - If the user has never gotten a loan before and,
+#-  if the user has never been to college before.
+# The client would like you to use the logical NOT operator that will
+# compare the condtions and return false. If the result is false, the client
+# would also like the program to print the message "congrats! you've gotten the scholarship."
+# the client has given you the choice on how to enter data for your function.
+# you may enter data using input or pass in data into your function as parameters. 
+
+
+def loan_acceptance(yes, no):
+    loan = input('Have you recieved a loan before?')
+    if (loan_acceptance == no):
+        print("Have you gotten into college before?")
+    elif (loan_acceptance == no):
+        print("congrats! you've gotten the scholarship.")
+    else:
+        print('Sorry, you do not qualify for this scholarship.')
