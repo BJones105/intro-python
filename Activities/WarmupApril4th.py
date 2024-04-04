acdcd390ce3a1faf3a295bd612f6ed3c9ecdf749
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-04-04 17:01:17 +0000

    April 14th Completed

---
+++ Activities/WarmupApril4th.py
@@ -0,0 +1,19 @@
+1.
+# A while loop will repeat a set of instructions as long as its true
+2.
+# Iterator is the start of a loop
+3.
+def message():
+while msg == 'hello':
+    print(msg)
+    msg = input('type a new message:')
+    if msg ='goodbye':
+            print('ending loop.')
+
+def checkBday():
+    bday = '828'
+    userInput = input('what is your bday?')
+    while userInput != bday:
+         userInput = ('Please enter the correct bday')
+         if userInput == bday:
+            print('congrats! happy bday')
