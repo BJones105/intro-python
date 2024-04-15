a6f413d6668c629dfd5bbf21d0597b45da9c7eb7
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-04-15 16:04:44 +0000

    April ASync Complete

---
+++ Activities/PythonLoopLecture.py
@@ -0,0 +1,6 @@
+# A loop is a A for loop repeats commands a specific number of times and a while loop repeats until a condition becomes false.
+
+# For Loop - A loop that iterertes over a collection of data.
+# data can be lists [], and strings.
+
+# While Loop - Block of code taht repeats itself so long as a condition is true.
