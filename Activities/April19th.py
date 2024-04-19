9f92d08f9d711bd40aceea8e22b04ca21570f260
BJones105 <158313618+BJones105@users.noreply.github.com>
2024-04-19 16:17:36 +0000

    April 19th Assignment Completed

---
+++ Activities/April19th.py
@@ -0,0 +1,40 @@
+#1A. Visit the web page link below and read the following article on asking questions:
+# https://www.betterup.com/blog/how-to-ask-good-questions
+
+# 1B. Based on what you've read, in your own words, describe what a good
+# question is.
+
+'A good question is an question that gets straight to the point and is not confusing.'
+
+# 1C. In your opinion and in your own words, why do you think its
+# important to be able to ask good questions.
+'It is important to ask good questions because you can learn more information'
+# 2A. Visit the link web page below and complete the following learning style survey:
+# http://www.educationplanner.org/students/self-assessments/learning-styles-quiz.shtml
+
+#2B. Based on your completed survey results, which learning style are you? List, three (3)
+# things from the survey results that you feel best describe your learning style. 
+'I am a visual learner'
+'Have your eyesight checked on a regular basis.'
+'Use flashcards to learn new words.'
+'Try to visualize things that you hear or things that are read to you.'
+#2B. Based on your completed survey results, do you feel as though the result are arcurate in
+# reflecting your learning style? if yes, explain why, if no, explain why. Please write your
+# response in atleast three (3) complete sentences. 
+'I do believe the results are accurate. Since I was in elementary school i always been a more of a visual learner'
+'than anything else. I feel as if I do not see it, it will not leave a mark on my brain'
+#3A. Visit the  webp age link below and read the article on how to study:
+# https://summer.harvard.edu/blog/top-10-study-tips-to-study-like-a-harvard-student/
+
+# 3B. Which of the following tips do you feel you need to work on when studying? Explain
+# why in at least three (3) complete sentences.
+'when it comes to studying i feel as if got many flaws i need to work on.'
+'I tend to get distracted alot and tend to dose off when it come to studying.'
+'I also never really ask for help I just sit there and try to be on my own '
+# 3C. Based on your learning style results, and the arcticle you just read on study tips,
+# think about how you can apply the study tips to your learning style. Then, in complete sentences
+# write down four (4) steps you could take to effectively study that is specific you you. 
+'I could just start to plan ahead and make a time space to study '
+'I could also go in a room with less distractions '
+'I could find somebody im am frieds with to study and help me with the work'
+'then i can finally reward myself for working hard and studying'
